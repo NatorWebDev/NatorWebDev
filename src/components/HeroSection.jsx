052a191ff1fcd22fa3c44ca9fead7f3255b73e94
NatorWebDev <126914912+NatorWebDev@users.noreply.github.com>
2023-05-19 19:52:50 -0400

    working on hero section, finished the section, lefts the animations and responsive design

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { NavBar } from "./NavBar";
 import { useInView } from "react-intersection-observer";
+import logo from "../assets/Logo.png";
 
 export const HeroSection = ({ show, setShow }) => {
   const { ref, inView } = useInView({
@@ -13,10 +14,34 @@ export const HeroSection = ({ show, setShow }) => {
   return (
     <section
       id="home"
-      className="w-full h-screen bg-bgColor text-white"
+      className="w-full h-screen bg-bgColor text-white flex items-center justify-center"
       ref={ref}
     >
       <NavBar show={show} />
+
+      <div className="h-384 w-full px-64 flex justify-between items-center">
+        <div className="w-1/2 h-full flex flex-col">
+          <h2 className="text-4xl mt-32">Luis Velasquez</h2>
+          <h1 className="text-5xl mt-48">Web Developer</h1>
+          <h2 className="text-5xl mt-4">Front-End</h2>
+          <div className="flex w-full justify-start mt-64 items-center text-xl">
+            <div className="bg-secondary px-12 py-4 rounded-xl cursor-pointer transition-all duration-200 hover:bg-primary">
+              See my work
+            </div>
+            <div className="text-secondary pl-32 cursor-pointer transition-all duration-200 hover:text-primary">
+              Contact
+            </div>
+          </div>
+          <div className="pt-32 opacity-75 ">
+            in the digital world, everyone needs a website
+          </div>
+        </div>
+        <div className="w-1/2 h-full flex items-center justify-end relative">
+          <img src={logo} className="h-384" alt="logo" />
+          <div className="w-128 h-128 bg-secondary right-0 bottom-0 absolute rounded-full translate_full"></div>
+          <div className="w-64 h-64 bg-primary left-0 bottom-0 absolute rounded-full translate_full"></div>
+        </div>
+      </div>
     </section>
   );
 };
