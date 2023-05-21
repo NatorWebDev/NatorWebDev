7c9b3ac27410ba22582400c19939c8b3ff87b508
NatorWebDev <126914912+NatorWebDev@users.noreply.github.com>
2023-05-21 18:13:52 -0400

    started projectssection, added data for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
+import { projects } from "../projectsData/data";
 
 export default function Projects({ setShow }) {
   const { ref, inView } = useInView({
@@ -10,8 +11,18 @@ export default function Projects({ setShow }) {
   }, [inView]);
 
   return (
-    <section id="projects" ref={ref} className="w-full h-screen bg-bgColor">
-      Projects
+    <section id="projects" ref={ref} className="w-full h-768 bg-bgColor relative z-20 flex flex-col text-white items-center">
+      <h2 className="text-3xl ">My Projects</h2>
+      <div className="w-full h-384 mt-48 px-64 flex">
+        {projects.map((project,index)=>(
+          <div className="h-full w-512 relative ml-8" key={index}>
+            <img src={project.image} alt="project" className="w-full h-full object-cover absolute top-0 left-0"/>
+            <div className={`w-full h-full bg-${(index%2 == 0)?'primary':'secondary'} opacity-30 absolute top-0 left-0`}></div>
+      
+          </div>
+        ))}
+        
+      </div>
     </section>
   );
 }
---
+++ src/projectsData/data.jsx
@@ -0,0 +1,19 @@
+import photo1 from "./imgs/photo1.png";
+import photo2 from "./imgs/photo2.png";
+
+export const projects = [
+    {
+        title:'PhotoGalleryNetwork',
+        image: photo1,
+        responsiveImg:'',
+        description:'',
+        url:'https://natorphotogallery.netlify.app/',
+    },
+    {
+        title:'Landing Page',
+        image:photo2,
+        responsiveImg:'',
+        description:'',
+        url:'',
+    }
+]
